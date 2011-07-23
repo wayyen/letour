d6a731dedf991c743b1004ab26ac8072e94a0713
Chen Way Yen <wayyen@gmail.com>
2011-07-23 12:01:06 +0800

    added slot_outlet attribute for Tag

--- src/com/twt/xtreme/TagData.java
+++ src/com/twt/xtreme/TagData.java
@@ -3,6 +3,7 @@ package com.twt.xtreme;
 public class TagData {
 	int slot_id;
 	String slot_name;
+	String slot_outlet;
 	
 	public int getSlot_id() {
 		return slot_id;
@@ -15,6 +16,12 @@ public class TagData {
 	}
 	public void setSlot_name(String slot_name) {
 		this.slot_name = slot_name;
+	}
+	public String getSlot_outlet() {
+		return slot_outlet;
+	}
+	public void setSlot_outlet(String slot_outlet) {
+		this.slot_outlet = slot_outlet;
 	}
 	
 }
